b3c27dd5c12f446392e65c06de262411a2324053
Kareem Ashraf <kareembahnasy17@gmail.com>
2024-03-10 23:09:57 +0200

    Add new `dto` data for auth

--- src/modules/auth/dto/auth.dto.ts
+++ src/modules/auth/dto/auth.dto.ts
@@ -1,4 +1,11 @@
 import {IsEmail, IsNotEmpty, IsString } from "@nestjs/class-validator";
+import { IsDate, IsEnum, IsOptional, MinLength } from "class-validator";
+
+export enum Gender {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
+}
+
 
 export class AuthDto {
 
@@ -11,8 +18,29 @@ export class AuthDto {
   @IsString()
   username: string;
 
+  @IsString()
+  @MinLength(3)
+  firstName: string;
+
+  @IsString()
+  @MinLength(3)
+  lastName: string;
+
+  @IsEnum(Gender)
+  gender: string;
+
+  @IsDate()
+  dateOfBirth: Date;
+  
+  @IsString()
+  @IsOptional()
+  country?: string;
+
   @IsNotEmpty()
   @IsString()
   password: string;
 
 }
+
+
+

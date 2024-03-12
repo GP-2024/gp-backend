737a769785562e41df89b032f94bc1257ab78857
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-03-12 10:38:41 +0200

    fix(auth): :bug: added userModule to authModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { FacebookStrategy } from './strategies';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [JwtModule.register({})],
+  imports: [JwtModule.register({}), UserModule],
   controllers: [AuthController],
   providers: [AuthService, FacebookStrategy],
 })

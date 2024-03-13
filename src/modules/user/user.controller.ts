811064fc049b048903a2163ac548a967e9a41ec7
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-03-13 18:04:45 +0200

    feat(User): :sparkles: added findOne and findAll controllers

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, ParseUUIDPipe } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UUIDParam } from 'src/decorators/uuid-param.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+  @Get(':id')
+  findOne(@UUIDParam('id') id: string) {
+    return this.userService.findOne(id);
+  }
 
-
-
+  // TODO: Add the rest of the CRUD operations After merge tables and entities
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,32 +1,47 @@
-import { Injectable } from "@nestjs/common";
-import { UserLogin } from "./entities/user-login-data.entity";
-import { Repository } from "typeorm";
-import { InjectRepository } from "@nestjs/typeorm";
-import { updateDtoUser } from "./_helpers/updateDto";
-import { UserAccount } from "./entities/user-account.entity";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserLogin } from './entities/user-login-data.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { updateDtoUser } from './_helpers/updateDto';
+import { UserAccount } from './entities/user-account.entity';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(UserLogin)
-        private readonly userLoginRepository: Repository<UserLogin>,
-        @InjectRepository(UserAccount)
-        private readonly userLoginAccountRepository: Repository<UserAccount>,
-    ) {
+  constructor(
+    @InjectRepository(UserLogin)
+    private readonly userLoginRepository: Repository<UserLogin>,
+    @InjectRepository(UserAccount)
+    private readonly userAccountRepository: Repository<UserAccount>,
+  ) {}
+
+  async findOne(id: string) {
+    const user = await this.userAccountRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+    return { data: user };
+  }
 
-    async createUserLoginData(userData: Partial<UserLogin>): Promise<UserLogin> {
-        updateDtoUser(userData);
-        const userLogin = this.userLoginRepository.create(userData);
-        console.log(userLogin);
-        return await this.userLoginRepository.save(userLogin);
-    }
+  // TODO: pagenation and filter
+  async findAll() {
+    const users = await this.userAccountRepository.findAndCount({});
+    return { data: users[0], total: users[1] };
+  }
 
-    async createUserAccount(userAccount: Partial<UserAccount>): Promise<UserAccount> {
-        const userLoginAccount = this.userLoginAccountRepository.create(userAccount);
-        console.log(userLoginAccount);
-        return await this.userLoginAccountRepository.save(userLoginAccount);
-    }
+  async createUserLoginData(userData: Partial<UserLogin>): Promise<UserLogin> {
+    updateDtoUser(userData);
+    const userLogin = this.userLoginRepository.create(userData);
+    console.log(userLogin);
+    return await this.userLoginRepository.save(userLogin);
+  }
 
-
+  async createUserAccount(userAccount: Partial<UserAccount>): Promise<UserAccount> {
+    const userLoginAccount = this.userAccountRepository.create(userAccount);
+    console.log(userLoginAccount);
+    return await this.userAccountRepository.save(userLoginAccount);
+  }
 }

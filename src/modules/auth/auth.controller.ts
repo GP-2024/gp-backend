210df8992742054e910a197508ba4f2101632ce8
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-03-15 04:39:57 +0200

    refactor(Auth): :memo: changed the route to local/auth

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, UseGuards, HttpStatus, HttpCode, Request }
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { SignUpDto } from './dto/sign-up.dto';
-import { SignInDto } from "./dto/sign-in.dto";
+import { SignInDto } from './dto/sign-in.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -20,14 +20,13 @@ export class AuthController {
     return { data: user };
   }
 
-  @Post('/signup')
+  @Post('/local/signup')
   @HttpCode(HttpStatus.CREATED)
   signUp(@Body() signUpDto: SignUpDto) {
     return this.authService.signUp(signUpDto);
   }
 
-
-  @Post('/signin')
+  @Post('/local/signin')
   @HttpCode(HttpStatus.OK)
   signIn(@Body() signUpDto: SignInDto) {
     return this.authService.signIn(signUpDto);

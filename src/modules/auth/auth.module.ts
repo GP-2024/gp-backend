11a71c21de79721c67842080d862054c9d9fb31c
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-03-15 04:42:16 +0200

    refactor: :dizzy: added Users entity to auth.module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,9 +4,16 @@ import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { FacebookStrategy } from './strategies';
 import { UserModule } from '../user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from '../user/entities/users.entity';
 
 @Module({
-  imports: [JwtModule.register({}), UserModule],
+  imports: [
+    JwtModule.register({}),
+    TypeOrmModule.forFeature([Users]), // add this line
+
+    UserModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService, FacebookStrategy],
 })

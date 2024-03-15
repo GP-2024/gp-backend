75f37794a9c268f4a8a7b83d1f9f73ac320d8619
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-03-15 04:47:23 +0200

    refactor(User): :rotating_light: changed dateOfBirth and gender to make them accept null values
    temp solution

--- src/modules/user/entities/users.entity.ts
+++ src/modules/user/entities/users.entity.ts
@@ -1,55 +1,52 @@
-import { Transform, TransformFnParams } from "class-transformer";
-import { IsDate, IsEnum, IsOptional, IsString, MinLength } from "class-validator";
-import GeneralEntity from "src/interfaces/generalEntity";
-import { Column, Entity } from "typeorm";
+import { Transform, TransformFnParams } from 'class-transformer';
+import { IsDate, IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
+import GeneralEntity from 'src/interfaces/generalEntity';
+import { Column, Entity } from 'typeorm';
 
 export enum Gender {
-    MALE = "MALE",
-    FEMALE = "FEMALE",
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
 }
 
-@Entity("users")
+@Entity('users')
 export class Users extends GeneralEntity {
+  @Column({ type: 'varchar', length: 15, unique: true })
+  @IsString()
+  username: string;
 
-    @Column({ type: "varchar", length: 15, unique: true })
-    @IsString()
-    username: string;
+  @Column({ type: 'varchar' })
+  @IsString()
+  password: string;
 
-    @Column({ type: "varchar" })
-    @IsString()
-    password: string;
+  @Column({ type: 'varchar', unique: true })
+  @IsString()
+  email: string;
 
-    @Column({ type: "varchar", unique: true })
-    @IsString()
-    email: string;
+  @Column({ type: 'varchar', length: 15 })
+  @IsString()
+  @MinLength(3)
+  firstName: string;
 
+  @Column({ type: 'varchar', length: 15 })
+  @IsString()
+  @MinLength(3)
+  lastName: string;
 
-    @Column({ type: "varchar", length: 15 })
-    @IsString()
-    @MinLength(3)
-    firstName: string;
+  @Column({ type: 'enum', enum: Gender, nullable: true })
+  @IsEnum(Gender)
+  gender: string;
 
-    @Column({ type: "varchar", length: 15 })
-    @IsString()
-    @MinLength(3)
-    lastName: string;
+  @Column({ type: 'timestamp', nullable: true })
+  @IsDate()
+  dateOfBirth: Date;
 
+  @Column({ type: 'timestamp', default: new Date() })
+  @IsDate()
+  @Transform(({ value }: TransformFnParams) => new Date(value))
+  joiningDate: Date;
 
-    @Column({ type: "enum", enum: Gender, nullable: true })
-    @IsEnum(Gender)
-    gender: string;
-
-    @Column({ type: "timestamp" })
-    @IsDate()
-    dateOfBirth: Date;
-
-    @Column({ type: "timestamp", default: new Date() })
-    @IsDate()
-    @Transform(({ value }: TransformFnParams) => new Date(value))
-    joiningDate: Date;
-
-    @Column({ type: "varchar", nullable: true })
-    @IsString()
-    @IsOptional()
-    country?: string;
+  @Column({ type: 'varchar', nullable: true })
+  @IsString()
+  @IsOptional()
+  country?: string;
 }

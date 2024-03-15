cc0475dfe6a9104d9b0c990adcdfeb465b3da0da
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-03-15 04:43:02 +0200

    fix(Auth): :fire: added more validation in signup function

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,19 +1,35 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { UserService } from "../user/user.service";
-import { SignUpDto } from "./dto/sign-up.dto";
-import { JwtPayload, Tokens } from "./types";
-import * as argon2 from "argon2";
-import { updateDTO } from "./_helpers/updateDto";
-import * as process from "process";
-import { SignInDto } from "./dto/sign-in.dto";
-import { JwtService } from "@nestjs/jwt";
+import { BadRequestException, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { SignUpDto } from './dto/sign-up.dto';
+import { JwtPayload, Tokens } from './types';
+import * as argon2 from 'argon2';
+import { updateDTO } from './_helpers/updateDto';
+import * as process from 'process';
+import { SignInDto } from './dto/sign-in.dto';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from '../user/entities/users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService, private jwtService: JwtService,
- ) {}
+  constructor(
+    private readonly userService: UserService,
+    @InjectRepository(Users)
+    private readonly userRepository: Repository<Users>,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(signUpDto: SignUpDto): Promise<{ message: string }> {
+    const userExist = await this.userRepository.findOne({
+      where: {
+        email: signUpDto.email,
+      },
+    });
+
+    if (userExist) {
+      throw new BadRequestException('This user already exists');
+    }
     const hashType: 0 | 1 | 2 = +process.env.ARGON_TYPE as 0 | 1 | 2;
 
     const hash = await argon2.hash(signUpDto.password, {
@@ -26,23 +42,23 @@ export class AuthService {
 
     updateDTO.updateDtoAuth(hash, signUpDto);
 
-      await this.userService.createUserAccount(signUpDto);
+    await this.userService.createUserAccount(signUpDto);
     return { message: 'User Registered Successful' };
   }
 
-  async signIn(signInDto:SignInDto) : Promise<Tokens>{
-
+  async signIn(signInDto: SignInDto): Promise<Tokens> {
     const email = signInDto.email;
     const passwordPT = signInDto.password;
 
     const userData = (await this.userService.findUserByEmail(email)).data;
 
-    if (!userData) {throw new UnauthorizedException('This user not exist')}
+    if (!userData) {
+      throw new UnauthorizedException('This user not exist');
+    }
 
     const { password: hashedPassword, id: userId } = userData;
 
-
-    if (!await argon2.verify(hashedPassword,passwordPT)) {
+    if (!(await argon2.verify(hashedPassword, passwordPT))) {
       throw new UnauthorizedException('Incorrect password');
     }
 
@@ -66,12 +82,9 @@ export class AuthService {
       }),
     ]);
 
-      return {
+    return {
       access_token: at,
       refresh_token: rt,
     };
   }
-
-
-
 }

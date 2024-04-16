4dd5705d99dd31ecbdea59622ef612f70c6ab94d
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-04-16 04:07:40 +0200

    Update import path for trefleController in api.module.ts

--- src/modules/apis/api.module.ts
+++ src/modules/apis/api.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { trefleController } from './trefle/trefleController';
-import { PerenualController } from './perenual/perenual/perenual.controller';
+import { PerenualController } from './perenual/perenual.controller';
+import { PerenualService } from './perenual/perenual.service';
+import { PerenualPlants } from './perenual/entities/perenual-details.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([PerenualPlants])],
   controllers: [trefleController, PerenualController],
-  providers: [],
+  providers: [PerenualService, PerenualController],
 })
 export class ApiModule {}

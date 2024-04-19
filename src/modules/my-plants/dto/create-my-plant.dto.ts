5e951cbb2c882c9757ccba2d7e22e3c215cc69a8
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-04-20 01:35:42 +0200

    Add validation constraints to CreateMyPlantDto

--- src/modules/my-plants/dto/create-my-plant.dto.ts
+++ src/modules/my-plants/dto/create-my-plant.dto.ts
@@ -1,1 +1,8 @@
-export class CreateMyPlantDto {}
+import { IsInt, IsPositive } from 'class-validator';
+import { GeneralDto } from 'src/common/interfaces/generalDto';
+
+export class CreateMyPlantDto extends GeneralDto {
+  @IsInt()
+  @IsPositive()
+  plantId: number;
+}

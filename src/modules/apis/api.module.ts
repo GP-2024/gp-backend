47d3866cb1ad4084c3bd378bbd91a600a0122d91
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-04-19 19:26:36 +0200

    Add API controller and service for scrap module

--- src/modules/apis/api.module.ts
+++ src/modules/apis/api.module.ts
@@ -4,10 +4,12 @@ import { PerenualController } from './perenual/perenual.controller';
 import { PerenualService } from './perenual/perenual.service';
 import { PerenualPlants } from './perenual/entities/perenual-details.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiController } from './api.controller';
+import { ApiService } from './api.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PerenualPlants])],
-  controllers: [trefleController, PerenualController],
-  providers: [PerenualService, PerenualController],
+  controllers: [trefleController, PerenualController, ApiController],
+  providers: [PerenualService, ApiService],
 })
 export class ApiModule {}

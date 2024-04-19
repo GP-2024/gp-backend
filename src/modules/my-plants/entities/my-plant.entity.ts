62f9edd403c41b578c2cabf907c0665352a3c5e2
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-04-20 01:35:38 +0200

    Refactor MyPlants entity to add unique constraint on plant_id and createdBy columns

--- src/modules/my-plants/entities/my-plant.entity.ts
+++ src/modules/my-plants/entities/my-plant.entity.ts
@@ -1,15 +1,43 @@
+import { IsUUID } from 'class-validator';
 import GeneralEntity from 'src/common/interfaces/generalEntity';
 import { PerenualPlants } from 'src/modules/apis/perenual/entities/perenual-details.entity';
 import { Users } from 'src/modules/user/entities/users.entity';
-import { Entity, JoinColumn, JoinTable, ManyToMany, OneToOne } from 'typeorm';
+import { Entity, Column, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToOne, PrimaryGeneratedColumn, Relation, Unique } from 'typeorm';
+import { UUID } from 'typeorm/driver/mongodb/bson.typings';
 
 @Entity('my_plants')
+@Unique(['plant_id', 'createdBy'])
 export class MyPlants extends GeneralEntity {
-  @OneToOne(() => Users, (user) => user.myPlants)
-  @JoinColumn({ name: 'username' })
-  user: Users;
+  @Column()
+  plant_id: number;
 
-  @ManyToMany(() => PerenualPlants)
-  @JoinTable()
-  plants: PerenualPlants[];
+  @Column()
+  createdBy: string;
+
+  @ManyToOne('PerenualPlants', { eager: true }) // Specify the related entity
+  @JoinColumn({ name: 'plant_id', referencedColumnName: 'id' }) // Specify the join column
+  Plant: PerenualPlants; // Define the property to hold the related entity
 }
+
+// @Entity('my_plants')
+// @Unique(['plant_id', 'createdBy'])
+// // TODO: ADD EXTENDS GeneralEntity
+// export class MyPlants {
+//   @PrimaryGeneratedColumn('uuid')
+//   @IsUUID('4')
+//   id: string;
+
+//   @Column()
+//   plant_id: number;
+
+//   @Column()
+//   createdBy: string;
+
+//   @ManyToOne('PerenualPlants', 'MyPlants')
+//   @JoinColumn({ name: 'plant_id', referencedColumnName: 'id' })
+//   Plant?: Relation<PerenualPlants>;
+
+//   @ManyToOne('Users', 'MyPlants')
+//   @JoinColumn({ name: 'createdBy', referencedColumnName: 'username' })
+//   User?: Relation<Users>;
+// }

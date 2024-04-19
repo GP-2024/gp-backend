41c386140c0c8edc80c8f031015068e12f00d399
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-04-20 01:35:50 +0200

    Add MyPlants entity to MyPlantsModule imports

--- src/modules/my-plants/my-plants.module.ts
+++ src/modules/my-plants/my-plants.module.ts
@@ -5,9 +5,10 @@ import { ApiModule } from '../apis/api.module';
 import { PerenualService } from '../apis/perenual/perenual.service';
 import { PerenualPlants } from '../apis/perenual/entities/perenual-details.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MyPlants } from './entities/my-plant.entity';
 
 @Module({
-  imports: [ApiModule, TypeOrmModule.forFeature([PerenualPlants])],
+  imports: [ApiModule, TypeOrmModule.forFeature([PerenualPlants, MyPlants])],
   controllers: [MyPlantsController],
   providers: [MyPlantsService, PerenualService],
 })

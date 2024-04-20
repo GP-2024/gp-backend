95ea38dd4a4e395e003f65d90d23699656f835c1
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-04-20 10:49:21 +0200

    Add LoggingMiddleware to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { ParseUUIDPipe, ValidationPipe } from '@nestjs/common';
 import { EntityNotFoundExceptionFilter } from './utlis/notFoundHandler';
 import helmet from 'helmet';
+import { LoggingMiddleware } from './utlis/requestLogger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {});
   app.use(helmet());
+  app.use(new LoggingMiddleware().use);
 
   app.useGlobalFilters(new EntityNotFoundExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));

6229509b36bb788da1c5062f0be1480c1c856d58
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-05-16 00:16:56 +0300

    feat:  throttler rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,13 +10,33 @@ import { MyPlantsModule } from './modules/my-plants/my-plants.module';
 import { BlogsModule } from './modules/blogs/blogs.module';
 import { QueueModule } from './modules/queues/queues.module';
 import { S3Module } from './modules/s3/s3.module';
-
+import { ThrottlerGuard, ThrottlerModule, seconds } from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
+import Redis from 'ioredis';
+import { APP_GUARD } from '@nestjs/core';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => TypeOrmConfig.getOrmConfig(configService),
+    }),
+
+    ThrottlerModule.forRoot({
+      /**
+       * This is will be for all the routes
+       * we can override this in the controller level
+       * @Throttle({ defualt: { limit: 20, ttl: 60 } })
+       */
+      throttlers: [{ limit: 5, ttl: seconds(60) }],
+
+      storage: new ThrottlerStorageRedisService(
+        new Redis({
+          host: process.env.REDIS_HOST,
+          port: +process.env.REDIS_PORT,
+          password: process.env.REDIS_PASSWORD,
+        }),
+      ),
     }),
 
     ApiModule,
@@ -28,6 +48,12 @@ import { S3Module } from './modules/s3/s3.module';
     S3Module,
   ],
   controllers: [AppController],
-  providers: [],
+
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}

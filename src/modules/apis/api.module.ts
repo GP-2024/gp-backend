1893802fde15dfa7208d47f1aa99e3ecafbe6ff3
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-06-11 02:33:16 +0300

    chore: Add MyPlantsService to ApiModule imports and providers

--- src/modules/apis/api.module.ts
+++ src/modules/apis/api.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { trefleController } from './trefle/trefleController';
 import { PerenualController } from './perenual/perenual.controller';
 import { PerenualService } from './perenual/perenual.service';
@@ -6,10 +6,14 @@ import { PerenualPlants } from './perenual/entities/perenual-details.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
+import { MyPlantsService } from '../my-plants/my-plants.service';
+import { MyPlantsModule } from '../my-plants/my-plants.module';
+import { MyPlants } from '../my-plants/entities/my-plant.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PerenualPlants])],
+  imports: [forwardRef(() => MyPlantsModule), TypeOrmModule.forFeature([PerenualPlants, MyPlants])],
   controllers: [trefleController, PerenualController, ApiController],
-  providers: [PerenualService, ApiService],
+  providers: [PerenualService, ApiService, MyPlantsService],
+  exports: [ApiService],
 })
 export class ApiModule {}

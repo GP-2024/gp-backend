037d56e0dd4566b608abcb2e5888c8da7d82c416
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-06-11 14:07:05 +0300

    chore: Add cache interceptor to PerenualController

--- src/modules/apis/perenual/perenual.controller.ts
+++ src/modules/apis/perenual/perenual.controller.ts
@@ -1,18 +1,21 @@
-import { Controller, Get, Param, Query, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Query, Request, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AtGuard } from 'src/common/guards';
 import { PerenualService } from './perenual.service';
 import { PerenualFilterDto } from './dto/perenual-filter.dto';
 import { Throttle } from '@nestjs/throttler';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 
 @Controller('perenual')
 @UseGuards(AtGuard)
 @Throttle({ default: { limit: 30, ttl: 60 } })
+@UseInterceptors(CacheInterceptor)
 export class PerenualController {
   constructor(private perenualService: PerenualService) {}
 
   // TODO: Implement Filter and Pagination
   @Get('/plants-details')
   findAll(@Query() queryParams: PerenualFilterDto, @Request() { user }) {
+    console.log('inside controller');
     return this.perenualService.findAll(queryParams, user.username);
   }
 

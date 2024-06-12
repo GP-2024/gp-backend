b3c865188d18781d8c3d0f7e570cfc57eef7d3f3
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-06-13 02:27:45 +0300

    feat: Add CacheModule import and CacheInvalidationService to my-plants.module.ts

--- src/modules/my-plants/my-plants.module.ts
+++ src/modules/my-plants/my-plants.module.ts
@@ -6,9 +6,11 @@ import { PerenualService } from '../apis/perenual/perenual.service';
 import { PerenualPlants } from '../apis/perenual/entities/perenual-details.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MyPlants } from './entities/my-plant.entity';
+import { CacheInvalidationService } from 'src/cache/cacheInvalidation.service';
+import { CacheModule } from 'src/cache/cache.module';
 
 @Module({
-  imports: [forwardRef(() => ApiModule), TypeOrmModule.forFeature([PerenualPlants, MyPlants])],
+  imports: [forwardRef(() => ApiModule), TypeOrmModule.forFeature([PerenualPlants, MyPlants]), CacheModule],
   controllers: [MyPlantsController],
   providers: [MyPlantsService, PerenualService],
 })

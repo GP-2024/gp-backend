c7ccb9636236286da198412e6d924e0e57b516a0
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-06-22 12:02:39 +0300

    refactor: added blog max and min length validation

--- src/modules/blogs/dto/post-blog.dto.ts
+++ src/modules/blogs/dto/post-blog.dto.ts
@@ -1,11 +1,16 @@
 import { IsNotEmpty, IsString } from '@nestjs/class-validator';
+import { Max, MaxLength, MinLength } from 'class-validator';
 
 export class postDTO {
   @IsNotEmpty()
   @IsString()
+  @MinLength(3)
+  @MaxLength(20)
   title: string;
 
   @IsNotEmpty()
   @IsString()
+  @MinLength(3)
+  @MaxLength(200)
   content: string;
 }
